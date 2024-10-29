c662273c26e51379ec4cc90b52941658599c3603
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-10-29 16:11:17 +0300

    Update

---
+++ SensiveProject.DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SensiveProject.EntityLayer.Concrete;
+namespace SensiveProject.DataAccessLayer.Abstract
+{
+	public interface ICategoryDal:IGenericDal<Category>
+	{
+	}
+}
